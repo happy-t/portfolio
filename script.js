ab525425355187dcb8a9af94acd499c25b1d2b31
= <=>
2023-08-11 22:05:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function visitLinux()
+{
+    window.open("https://github.com/happy-t/Shell_Project").focus();
+}
+
+function visitKlan()
+{
+    window.open("https://github.com/happy-t/K-Lan").focus();
+}
+
+function visitWa()
+{
+    window.open("https://happy-t.github.io/weather-app/").focus();
+}
+
+function visiitLearn_Sanskrit()
+{
+    window.open("https://github.com/happy-t/Learn_Sanskrit").focus();
+}
